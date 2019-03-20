a024d4c69e63b20afe8cc0b6818f5f2d28d9cc7c
RonnyRitscher <r.k.ritscher@web.de>
2019-03-20 12:35:05 +0100

    2019.03.20_12:30

--- app/src/main/java/de/proneucon/myrecyclerviewdemo/MainActivity.java
+++ app/src/main/java/de/proneucon/myrecyclerviewdemo/MainActivity.java
@@ -7,14 +7,26 @@ package de.proneucon.myrecyclerviewdemo;
  * <p>
  * Basteln der Klasse Person
  * Elemente angeben
+ *
+ *
+ * !! Wenn in dem ItemLayout die TextViews auf MatchConstraind gesetzt sind, wird die Liste nicht angezeigt!!!
+ * !! Lösung: die rechte Constraint des Items muss gelöscht werden, dann ist matchConstraind verwendbar!!!
  */
 
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
+import android.view.View;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -41,15 +53,83 @@ public class MainActivity extends AppCompatActivity {
                 new Person("Frank", "Hamburg"),
                 new Person("Paul", "Bremen"),
                 new Person("Mia", "Bonn"),
+                new Person("Moritz", "Hamburg"),
+                new Person("Max", "Berlin"),
+                new Person("Frank", "Hamburg"),
+                new Person("Paul", "Bremen"),
+                new Person("Mia", "Bonn"),
+                new Person("Moritz", "Hamburg"),
+                new Person("Max", "Berlin"),
+                new Person("Frank", "Hamburgasdasdasdasd"),
+                new Person("Paul", "Bremen"),
+                new Person("Mia", "Bonn"),
+                new Person("Moritz", "Hamburg"),
+                new Person("Max", "Berlin"),
+                new Person("Frank", "Hamburg"),
+                new Person("Paul", "Bremenasdasdasdasdasdasd"),
+                new Person("Mia", "Bonn"),
                 new Person("Moritz", "Hamburg")
         ));
 
         recyclerView = findViewById(R.id.recyclerView);
-        rvLayoutManager = new LinearLayoutManager(this);   //LAYOUT-MANAGER
 
-        //Adapter wird initialisiert/gebaut
-        rvAdapter = new RvAdapter();        //extern eine neue Klasse RvAdapter() definieren
-        recyclerView.setAdapter(rvAdapter); //rv den Adapter geben
+        //ITEM-DECORATION:    verändern der enthaltenen Items****************************************************************
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.set(10,10,10,10);  //setzt den äußeren Abstand
+
+            }
+
+            //Wir ändern die Items und lassen einen Rand anzeigen in Farbe
+            @Override
+            public void onDrawOver(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.onDrawOver(c, parent, state);
+                RecyclerView.LayoutManager layoutManager = parent.getLayoutManager(); //holt den Layoutmanager
+                //schleife für alle Elemente:
+                for(int i=0; i<parent.getChildCount();i++){
+                    View child = parent.getChildAt(i);
+                    //Farbe definieren
+                    Paint paintCyan = new Paint();
+                    paintCyan.setAntiAlias(true);
+                    paintCyan.setColor(Color.CYAN);
+                    paintCyan.setStyle(Paint.Style.STROKE);
+                    paintCyan.setStrokeWidth(5);
+
+                    //Da ab lev 21 surround with version control
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        //Canvas zeichnen - achte auch die richtige angabe
+                        c.drawRoundRect(        //c ist das Canvas
+                                layoutManager.getDecoratedLeft(child)+10,   //Abstand zu den anderen Elementen
+                                layoutManager.getDecoratedTop(child)+10,
+                                layoutManager.getDecoratedRight(child)-10,
+                                layoutManager.getDecoratedBottom(child)-10,
+                                20,  //Rundungen
+                                20,
+                                paintCyan  //Farbe
+                        );
+                    }
+                }
+            }
+        });
+
+        //LAYOUT-MANAGER:****************************************************************
+        /* layoutManager - normale Ansicht: */
+        rvLayoutManager = new LinearLayoutManager(this);
+
+        /* layoutManager - erweiterung: elemente nebeneinander *** reverse: false zeigt den ersten Eintrag, true springt auf den letzten Eintrag */
+//        rvLayoutManager = new LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false);
+
+        /* verwenden von GRID-LAYOUT anzeigen der Items über Grid nebeneinander (für bsp: Landscape) */
+//        rvLayoutManager = new GridLayoutManager(this , 2); //
+
+        //ADAPTER: Adapter wird initialisiert/gebaut****************************************************************
+        rvAdapter = new RvAdapter();                        //extern eine neue Klasse RvAdapter() definieren
+
+        //Übergeben von LAYOUT und ADAPTER****************************************************************
+        recyclerView.setLayoutManager(rvLayoutManager);     //rv den LayoutManager übergeben
+        recyclerView.setAdapter(rvAdapter);                 //rv den Adapter geben
 
 
     }
--- app/src/main/java/de/proneucon/myrecyclerviewdemo/RvAdapter.java
+++ app/src/main/java/de/proneucon/myrecyclerviewdemo/RvAdapter.java
@@ -1,48 +1,59 @@
 package de.proneucon.myrecyclerviewdemo;
 
+/**
+ * Adapter für die RecyclerView
+ */
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * Adapter für die RecyclerView
- */
 
+class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder>  {
 
+    private static final String TAG = RvAdapter.class.getSimpleName(); //LOG-TAG
 
-class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
 
 
     @NonNull
     @Override
     public RvAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         //wir brauchen die View  über das erstellte Layouut für die Items der RV
-        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_recyclerview_layout , null);
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_recyclerview_layout , null);
 
         return new ViewHolder(itemView);   // wir erzeugen ein neuen VH und geben diesen zurück
     }
 
     @Override
     public void onBindViewHolder(@NonNull RvAdapter.ViewHolder viewHolder, int i) {
-        //
+        // besogt die Daten aus der DummyListe
         viewHolder.itemName.setText(MainActivity.personArrayList.get(i).getName()); //Liest den Name aus der AL für jeden Eintrag
         viewHolder.itemCity.setText(MainActivity.personArrayList.get(i).getCity()); //Liest den City aus der AL für jeden Eintrag
+
+
+        //Hier können wir die Elemente anklicken
+        viewHolder.itemView.setOnClickListener( (v) -> {
+            Log.d(TAG, "onBindViewHolder: aus dem ViewAdapter :" + i + " wurde angewählt");
+        });
+
     }
 
     @Override
     public int getItemCount() {
-
         return MainActivity.personArrayList.size();  // Gibt die Größe der Liste an
     }
+
+
 
     //************************
     //Innere Klasse die die View hält und an die View zurück gibt:
     public class ViewHolder  extends RecyclerView.ViewHolder{
 
-        //MEMBER: Wir benötigen die Member
+        //MEMBER: Wir benötigen Member
         TextView itemName, itemCity;
 
         //CONSTRUCTOR
